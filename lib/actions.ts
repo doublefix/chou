48e726d5baa4f4d8cd54b70919a317596abe3f09
mahongqin <codepapercut@gmail.com>
2024-12-17 00:45:41 +0800

    Fix login

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,5 +1,3 @@
-import { redirect } from "next/navigation";
-
 export async function authenticate(
   prevState: string | undefined,
   formData: FormData
@@ -8,20 +6,21 @@ export async function authenticate(
   formData.forEach((value, key) => {
     formObject[key] = value as string;
   });
-  console.log(formObject.email);
-  console.log(formObject.password);
 
   const response = await fetch("/api/v1/token/access", {
     method: "POST",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       uname: formObject.email,
       passwd: formObject.password,
     }),
   });
 
-  console.log(response.ok);
   if (response.ok) {
-    redirect("/");
+    const data = await response.json();
+    if (data.redirect) {
+      window.location.href = data.redirect;
+    }
   } else {
     return "登录失败，请检查您的凭证。";
   }
