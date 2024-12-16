7a800890dbef189271491b34b121413114e7bdd4
mahongqin <codepapercut@gmail.com>
2024-12-16 20:39:10 +0800

    Fix access

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,26 +1,28 @@
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
 
 export async function authenticate(
   prevState: string | undefined,
   formData: FormData
 ) {
-  try {
-    const formObject: { [key: string]: string } = {};
-    formData.forEach((value, key) => {
-      formObject[key] = value as string;
-    });
-    console.log(formObject.email)
-    console.log(formObject.password)
+  const formObject: { [key: string]: string } = {};
+  formData.forEach((value, key) => {
+    formObject[key] = value as string;
+  });
+  console.log(formObject.email);
+  console.log(formObject.password);
 
-    // 模拟调用后端接口假设成功了
-    const loginSuccessful = true;
+  const response = await fetch("/api/v1/token/access", {
+    method: "POST",
+    body: JSON.stringify({
+      uname: formObject.email,
+      passwd: formObject.password,
+    }),
+  });
 
-    if (loginSuccessful) {
-      redirect('/');
-    } else {
-      return '登录失败，请检查您的凭证。';
-    }
-  } catch (error) {
-    throw error;
+  console.log(response.ok);
+  if (response.ok) {
+    redirect("/");
+  } else {
+    return "登录失败，请检查您的凭证。";
   }
 }
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
+  
+  /* 打包导出 */
+  // output: 'export',
+
+  /* 将请求代理到后端，开发时使用 */
+  async rewrites() { 
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8080/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
