b301d6317846f72f1e475832249ee9a152fb16f5
mahongqin <codepapercut@gmail.com>
2024-12-17 19:46:04 +0800

    Fix tailwind config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { cn } from "@/lib/utils"
+import { fontMono, fontSans } from "@/lib/fonts"
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -25,7 +27,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(
+          "min-h-svh bg-background font-sans antialiased",
+          fontSans.variable,
+          fontMono.variable
+        )}
       >
         {children}
       </body>
