2efef77d59619699cdbd2f6e6a89af4be77445f1
mahongqin <codepapercut@gmail.com>
2025-01-08 15:30:57 +0800

    Fix join

--- components/login/components/join.tsx
+++ components/login/components/join.tsx
@@ -30,7 +30,6 @@ export function JoinForm() {
       const result = await mutate("/api/v1/join", () => join(formData));
 
       if (result.code === 2001000) {
-        console.log("注册成功");
         setErrorMessage(null);
         return;
       }
@@ -72,6 +71,12 @@ export function JoinForm() {
                 <Label htmlFor="password">Password</Label>
               </div>
               <Input id="password" name="password" type="password" required />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="username">Username</Label>
+              </div>
+              <Input id="username" name="username" type="username" required />
             </div>
             {errorMessage && (
               <p className="text-sm text-red-500">{errorMessage}</p>
