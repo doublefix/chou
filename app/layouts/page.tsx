9bca1b73e1e7013940787193fab3501dc2a83f18
mahongqin <codepapercut@gmail.com>
2025-01-15 17:48:35 +0800

    Add page example

---
+++ app/layouts/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <main style={{ padding: "20px", textAlign: "center" }}>
+      <h1>欢迎来到我的 Next.js 应用！</h1>
+      <p>这是一个简单的示例页面。</p>
+    </main>
+  );
+};
+
+export default HomePage;
