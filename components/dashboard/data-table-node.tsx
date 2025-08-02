99ee4bfbd9e757e8feeb978cabf2286ec7b187a2
mahongqin <codepapercut@gmail.com>
2025-08-02 19:57:17 +0800

    Fix show data

--- components/dashboard/data-table-node.tsx
+++ components/dashboard/data-table-node.tsx
@@ -550,7 +550,7 @@ export function DataTable({
                   />
                 </SelectTrigger>
                 <SelectContent side="top">
-                  {[10, 20, 30, 40, 50].map((pageSize) => (
+                  {[1, 2, 10, 20, 30, 40, 50].map((pageSize) => (
                     <SelectItem key={pageSize} value={`${pageSize}`}>
                       {pageSize}
                     </SelectItem>
